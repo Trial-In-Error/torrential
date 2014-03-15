e369c4f9a5aca7571d6704d651ea27aeee7a836c
Borogarr <kgonzalez99@ufl.edu>
2014-03-15 11:43:53 -0400

    test

--- peerProcess.java
+++ peerProcess.java
@@ -1,5 +1,25 @@
+import java.io.*;
 
+public class peerProcess {
+	
 
 public static void main(String [] args) {
+
+}
+
+
+public void initialize(int peerID) {
+
+	BufferedReader reader = new BufferedReader(
+				new FileReader("Common.cfg"));
+	
+	String line = null;
+	while ((line = reader.readLine()) != null) {
+		
+
+}
+
+
+
 
 }
