4377219569abc14e7ec5202787de38d3a93ba76b
Borogarr <kgonzalez99@ufl.edu>
2014-03-15 14:57:13 -0400

    plz wurk

--- peerProcess.java
+++ peerProcess.java
@@ -77,8 +77,12 @@ public class peerProcess {
 		}*/
 	}
 
-	
-	public void initialize(int peerID) {
+	private void intialize() {
+		setupConstants();
+		setupConnections();
+	}
+
+	public void setupConstants(int peerID) {
 
 		File file = new File("Common.cfg");
 		File file = new File("PeerInfo.cfg");
@@ -118,20 +122,42 @@ public class peerProcess {
 				port_Temp = scanner.next();
 				hasFile_Temp = scanner.next();
 				
+				setupDirectory(peerID_Temp);
 				peerDict.put(peerID_Temp, peerData(peerID_Temp, host_Temp,
 													port_Temp, hasFile_Temp));
-													
 				
-			
-		
 			}
 		}
-		catch {
+		catch(FileNotFoundException e) {
 			e.printStackTrace();
 		}
 		sc.close();
 		
 	}
+	
+	private void setupDirectory(ID) {
+		File dir = new File("/peer_[ID]");
+		dir.mkdir();
+	}
+	
+	private void setupLogFiles(ID) {
+		try {
+			FileWriter fstream = new FileWriter("log_peer_[ID].log");
+			BufferedWriter out = new BufferedWriter(fstream);
+			out.write("Log File for peer "+ID+" has been generated.");
+			out.close();
+		}
+		catch(Exception e) {
+			system.err.println("Error: " + e.getMessage());
+		}
+	}
+	
+	private void setupConnections() {
+	
+		//Fill in...
+	
+	}
+
 
 	public void handleMessage(/*pass in message*/) {
 	//actions in response to receiving message
